1f3214899d39cfa94415ff0b3d0c70f37dd738c9
masalab <masa_tale-gate_te2ss@outlook.jp>
2020-03-29 21:54:23 +0900

    modefied

--- Bookers1/app/controllers/books_controller.rb
+++ Bookers1/app/controllers/books_controller.rb
@@ -8,11 +8,12 @@ class BooksController < ApplicationController
   end
 
   def create
-  	@books = Book.all
-  	book = Book.new(book_params)
-  	if book.save
-  		redirect_to show_path(book.id), notice: 'Book was successfully created.' #jump to showpage
+  	@book = Book.new(book_params) #index view in create action
+  	if @book.save
+  		flash[:notice] = "Book was successfully created."
+  		redirect_to book_path(@book.id)
   	else
+  		@books = Book.all
   		render action: :index
   	end
   end
@@ -26,9 +27,10 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(params[:id])
-  	if book.update(book_params)
-  		redirect_to book_path(book.id), notice: 'Book was successfully updated' #jump to showpage
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+  		flash[:notice] = "Book was successfully updated."
+  		redirect_to book_path(@book.id)
   	else
   		render action: :edit
   	end
@@ -37,7 +39,8 @@ class BooksController < ApplicationController
   def destroy
   	book = Book.find(params[:id])
   	book.destroy
-  	redirect_to '/books', notice: 'Book was successfully destroyed'
+  	flash[:notice] = "Book was successfully destroyed."
+  	redirect_to books_path
   end
 
   private
--- Bookers1/config/routes.rb
+++ Bookers1/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-
+	resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'books#top'
-  get 'books' => 'books#index', as: 'book' #used as 「book_path」
-  get 'books/:id' => 'books#show', as: 'show' #used as 「show_path」
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  post 'books' => 'books#create' #linked in index.html.erb 「Newpost part」 url: '/books'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  root 'books#top' 
+  #get 'books' => 'books#index', as: 'books' #used as 「books_path」
+  #get 'books/:id' => 'books#show', as: 'book' #used as 「book_path」
+  #get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  #post 'books' => 'books#create' #linked in index.html.erb 「Newpost part」 url: '/books'
+  #patch 'books/:id' => 'books#update', as: 'book'
+  #delete 'books/:id' => 'books#destroy', as: 'book'
 end
